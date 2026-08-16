4b2f16b662c4678b9af36a9ab63aa5cd8e925c92
wvhugoliyh <vvhugoliyh@gmail.com>
2026-08-16 16:57:51 +0100

    Add nodes package

---
+++ src/flintmc/nodes/expression.py
@@ -0,0 +1,33 @@
+from dataclasses import dataclass
+from typing import Literal
+
+# Type aliases are used to reduce repetitive code
+type numeric_value = MathBinaryOp | int | float | str
+type boolean_value = LogicBinaryOp | Comparison | bool
+
+
+@dataclass
+class MathBinaryOp:
+  opnd_1: numeric_value
+  opr: Literal["+", "-", "*", "/", "^"]
+  opnd_2: numeric_value
+
+@dataclass
+class MathNeg:
+  opnd: numeric_value
+
+@dataclass
+class LogicBinaryOp:
+  opnd_1: boolean_value
+  opr: Literal["||", "&&"]
+  opnd_2: boolean_value
+
+@dataclass
+class LogicNot:
+  opnd: boolean_value
+
+@dataclass
+class Comparison:
+  cmpnd_1: numeric_value
+  cmpr: Literal["==", "!=", "<", ">", "<=", ">="]
+  cmpnd_2: numeric_value
---
+++ src/flintmc/nodes/statements.py
@@ -0,0 +1,28 @@
+from dataclasses import dataclass
+from flintmc.nodes.expression import numeric_value
+from flintmc.nodes.conditional import If, Elif, Else
+
+# Type aliases are used to reduce repetitive code
+type stmt = Assignment | FuncCall | Conditional | Repeat
+
+
+@dataclass
+class Assignment:
+  var_id: str
+  value: numeric_value
+
+@dataclass
+class FuncCall:
+  func_id: str
+  args: list[numeric_value]
+
+@dataclass
+class Conditional:
+  if_stmt: If
+  elif_stmts: list[Elif]
+  else_stmt: Else | Literal[None]
+
+@dataclass
+class Repeat:
+  repeat_cnt: int
+  stmts: list[stmt]
