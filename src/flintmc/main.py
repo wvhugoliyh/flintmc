5b61dfff31d9161a91c3e4ec2768f6b0353f21d1
wvhugoliyh <vvhugoliyh@gmail.com>
2026-08-18 14:00:13 +0100

    Fix: Fix error

--- src/flintmc/main.py
+++ src/flintmc/main.py
@@ -9,6 +9,6 @@ app = typer.Typer()
 
 @app.command()
 def main(source_path: str, dest_path: str):
-  parser = Lark.open("grammar.lark", rel_to=__file__, parser="lalr", transformer=FlintParser)
+  parser = Lark.open("grammar.lark", rel_to=__file__, parser="lalr", transformer=FlintParser())
   source_path_obj = Path(source_path).resolve()
   print(parser.parse(source_path_obj.read_text()))
