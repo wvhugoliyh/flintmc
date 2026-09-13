424c925ca316ee32577b14fd81056fda2196c3c9
wvhugoliyh <vvhugoliyh@gmail.com>
2026-09-13 13:18:16 +0100

    Refactor: Add type annotations and docstrings

--- src/flintmc/main.py
+++ src/flintmc/main.py
@@ -1,3 +1,5 @@
+""" FlintMC is a CLI tool used to transpile Flint code into Minecraft datapacks. """
+
 from pathlib import Path
 
 import typer
@@ -8,7 +10,7 @@ from flintmc.flint_parser import FlintParser
 app = typer.Typer()
 
 @app.command()
-def main(source_path: str, dest_path: str):
+def main(source_path: str, dest_path: str) -> None:
   parser = Lark.open("grammar.lark", rel_to=__file__, parser="lalr", transformer=FlintParser())
   source_path_obj = Path(source_path).resolve()
   print(parser.parse(source_path_obj.read_text()))
