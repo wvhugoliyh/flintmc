53fd0fc58116206c8ad4d69ef71fee3ee6b9ade6
wvhugoliyh <vvhugoliyh@gmail.com>
2026-09-16 15:54:13 +0100

    Refactor and fix namespace being given as positional argument

--- src/flintmc/main.py
+++ src/flintmc/main.py
@@ -1,4 +1,4 @@
-""" FlintMC is a CLI tool used to transpile Flint code into Minecraft datapacks. """
+""" A tool to transpile Flint code into Minecraft datapacks. """
 
 from pathlib import Path
 
@@ -12,7 +12,13 @@ app = typer.Typer()
 
 @app.command()
 def main(source_path: str, dest_path: str) -> None:
-  parser = Lark.open("grammar.lark", rel_to=__file__, parser="lalr", transformer=FlintParser())
+  parser = Lark.open(
+    "grammar.lark",
+    rel_to=__file__,
+    parser="lalr",
+    transformer=FlintParser()
+  )
+
   source_path_obj = Path(source_path).resolve()
   ast = parser.parse(source_path_obj.read_text())
-  print(sort_ast(ast, "test"))
+  print(sort_ast(ast, namespace="test"))
